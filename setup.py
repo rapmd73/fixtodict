2014336944a5d3713f48a50dd4e19b0af76840be
Filippo Costa <filippocosta.italy@gmail.com>
2020-03-11 17:07:35 +0100

    Fix license classifier

--- setup.py
+++ setup.py
@@ -19,7 +19,7 @@ setup(
     classifiers=[
         'Development Status :: 5 - Production/Stable',
         'Intended Audience :: Developers',
-        'License :: OSI Approved :: Apache License',
+        'License :: OSI Approved :: Apache Software License',
         'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
